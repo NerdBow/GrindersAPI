805789d26e5c3a5574da25cd7ac43d0b175b773e
NerdBow <108768934+NerdBow@users.noreply.github.com>
2024-12-02 16:14:34 -0500

    Add base for log endpoint

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
@@ -10,9 +11,29 @@ func (handler *Handler) ServeHTTP(writer http.ResponseWriter, request *http.Requ
 	writer.Write([]byte("Hello"))
 }
 
+type LogHandler struct{}
+
+func (handler *LogHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	switch request.Method {
+	case http.MethodPost:
+		fmt.Println("Post")
+
+	case http.MethodGet:
+		fmt.Println("Get")
+
+	case http.MethodPut:
+		fmt.Println("Put")
+
+	case http.MethodDelete:
+		fmt.Println("Delete")
+	}
+}
+
 func main() {
 	mux := http.NewServeMux()
 	mux.Handle("/", &Handler{})
+
+	mux.Handle("/log/", &LogHandler{})
 
 	http.ListenAndServe(":8080", mux)
 }
