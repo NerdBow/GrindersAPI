7d7941eea3268295d3d7e2cfeb824edb00928c2e
NerdBow <108768934+NerdBow@users.noreply.github.com>
2024-12-08 08:55:47 -0500

    Change Date field to int64 & add Validate method

--- api/logs/model.go
+++ api/logs/model.go
@@ -1,16 +1,48 @@
 package logs
 
 import (
+	"fmt"
 	"time"
 )
 
 // The struct and date format for each log in the system
 type Log struct {
-	Id       int
-	Date     time.Time
-	Duration time.Duration
-	Name     string
-	Category string
-	Tags     []string
-	UserId   int
+	Id       int           `json:"id"`
+	Date     int64         `json:"date"`
+	Duration time.Duration `json:"duration"`
+	Name     string        `json:"name"`
+	Category string        `json:"category"`
+	Tags     []string      `json:"tags"`
+	UserId   int           `json:"userId"`
+}
+
+func (log Log) Validate() bool {
+	if log.Id == 0 {
+		return false
+	}
+
+	if log.Date == 0 {
+		return false
+	}
+
+	if log.Duration == 0 {
+		return false
+	}
+
+	if log.Name == "" {
+		return false
+	}
+
+	if log.Category == "" {
+		return false
+	}
+
+	if log.UserId == 0 {
+		return false
+	}
+	return true
+}
+
+func (log Log) String() string {
+	return fmt.Sprintf("Id: %d\nDate: %s\nDuration: %d\nName: %s\nCategory: %s\n", log.Id, time.Unix(log.Date, 0), log.Duration, log.Name, log.Category)
 }
