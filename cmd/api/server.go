40a595980425a3280a08f4014cbd38931a0dd1ff
NerdBow <108768934+NerdBow@users.noreply.github.com>
2024-12-08 11:33:26 -0500

    Add POST engpoiint for /log/

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"github.com/NerdBow/GrindersAPI/api/logs"
 	"net/http"
 )
 
@@ -16,9 +18,27 @@ type LogHandler struct{}
 func (handler *LogHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
 	switch request.Method {
 	case http.MethodPost:
+		decoder := json.NewDecoder(request.Body)
+		var requestLog logs.Log
+		err := decoder.Decode(&requestLog)
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		if !requestLog.Validate() {
+			writer.WriteHeader(http.StatusBadRequest)
+			writer.Write([]byte("400 Bad Request"))
+		}
+
 		fmt.Println("Post")
 
 	case http.MethodGet:
+		logId := request.URL.Path[5:]
+
+		fmt.Println(request.URL.Path, logId)
+
 		fmt.Println("Get")
 
 	case http.MethodPut:
@@ -28,7 +48,6 @@ func (handler *LogHandler) ServeHTTP(writer http.ResponseWriter, request *http.R
 		fmt.Println("Delete")
 	}
 }
-
 func main() {
 	mux := http.NewServeMux()
 	mux.Handle("/", &Handler{})
