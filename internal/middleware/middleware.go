09aed2c79f816b749eaaa1410f950542edcc0f6e
NerdBow <108768934+NerdBow@users.noreply.github.com>
2025-02-13 11:34:29 -0500

    Add logging middleware

--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -1,11 +1,14 @@
 package middleware
 
 import (
+	"log"
 	"net/http"
 )
 
-func SetHeader() http.HandlerFunc {
+func LoggingMiddleware(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s call on endpoint %s", r.Method, r.URL)
 		w.Header().Set("Content-Type", "application/json")
+		next(w, r)
 	}
 }
