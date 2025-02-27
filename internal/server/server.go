bb447bf8937af9fe567172b94eb4dd0b3068238b
NerdBow <108768934+NerdBow@users.noreply.github.com>
2025-02-27 11:39:50 -0500

    Add database & port to env

--- internal/server/server.go
+++ internal/server/server.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/NerdBow/GrindersAPI/internal/database"
@@ -21,33 +22,44 @@ func Run() {
 
 	mux := http.NewServeMux()
 
-	db, err := database.NewSqlite3DB()
+	sqlService := os.Getenv("DATABASE")
 
-	if err != nil {
-		fmt.Println(err)
-		return
+	var userService service.UserService
+	var userLogService service.UserLogService
+
+	switch sqlService {
+	case "sqlite3":
+		db, err := database.NewSqlite3DB()
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		err = db.CreateTables()
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		userService = service.NewUserService(db)
+		userLogService = service.NewUserLogService(db)
+		log.Println("Sqlite3 is being used.")
+	default:
+		log.Fatal("No database specification found in the .env")
 	}
-
-	err = db.CreateTables()
-
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	userService := service.NewUserService(db)
-	userLogService := service.NewUserLogService(db)
 
 	addRoutes(mux, userService, userLogService)
 
 	server := http.Server{
-		Addr:         ":8080",
+		Addr:         ":" + os.Getenv("PORT"),
 		Handler:      mux,
 		ReadTimeout:  5 * time.Second,
 		WriteTimeout: 5 * time.Second,
 	}
 
-	log.Println("Server started.")
+	log.Printf("Server started on port %s", server.Addr)
 
 	log.Fatal(server.ListenAndServe())
 }
