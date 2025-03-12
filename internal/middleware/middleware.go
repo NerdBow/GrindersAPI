c1d50b447d6a7af72eda700ebf9cd439f514681f
NerdBow <108768934+NerdBow@users.noreply.github.com>
2025-03-12 10:24:27 -0400

    Add middleware for logging endpoint & ip also util func for dealing with errors

--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -1,11 +1,26 @@
 package middleware
 
 import (
+	"log"
 	"net/http"
 )
 
-func SetHeader() http.HandlerFunc {
+func SetLog(handler http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("IP: %-22s Endpoint: %s\n", r.RemoteAddr, r.URL)
+		handler(w, r)
+	}
+}
+
+func SetHeader(handler http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
+		handler(w, r)
 	}
+}
+
+func HandleError(w http.ResponseWriter, err error, statusCode int, message string) {
+	log.Printf("%s\n", err)
+	w.WriteHeader(statusCode)
+	w.Write([]byte(message))
 }
