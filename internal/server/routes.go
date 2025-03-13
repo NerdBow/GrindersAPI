d45255f7ae19cee3d67845ff9be93121ee96545b
NerdBow <108768934+NerdBow@users.noreply.github.com>
2025-03-12 21:02:38 -0400

    Add logging to the route and user/signup endpoint

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -1,15 +1,19 @@
 package server
 
 import (
-	"github.com/NerdBow/GrindersAPI/internal/handler"
-	"github.com/NerdBow/GrindersAPI/internal/service"
 	"net/http"
+
+	"github.com/NerdBow/GrindersAPI/internal/handler"
+	"github.com/NerdBow/GrindersAPI/internal/middleware"
+	"github.com/NerdBow/GrindersAPI/internal/service"
 )
 
 // addRoutes adds all the handlers for each route to the provided mux.
 func addRoutes(mux *http.ServeMux, userService service.UserService, userLogService service.UserLogService) {
-	mux.HandleFunc("POST /user/{id}/log", handler.HandleUserLogPost(userLogService))
-	mux.HandleFunc("GET /user/{id}/log", handler.HandleUserLogGet(userLogService))
-	mux.HandleFunc("PUT user/{id}/log", handler.HandleUserLogUpdate(userLogService))
-	mux.HandleFunc("DELETE user/{id}/log", handler.HandleUserLogDelete(userLogService))
+	mux.HandleFunc("POST /user/signup", middleware.SetLog(handler.HandleUserSignIn(userService)))
+
+	mux.HandleFunc("POST /user/{id}/log", middleware.SetLog(handler.HandleUserLogPost(userLogService)))
+	mux.HandleFunc("GET /user/{id}/log", middleware.SetLog(handler.HandleUserLogGet(userLogService)))
+	mux.HandleFunc("PUT /user/{id}/log", middleware.SetLog(handler.HandleUserLogUpdate(userLogService)))
+	mux.HandleFunc("DELETE /user/{id}/log", middleware.SetLog(handler.HandleUserLogDelete(userLogService)))
 }
