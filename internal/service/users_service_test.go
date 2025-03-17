c037f380a89d3bec4120421fe241c08f629a2093
NerdBow <108768934+NerdBow@users.noreply.github.com>
2025-03-16 20:14:10 -0400

    Update test for helpers funcs in UserService

--- internal/service/users_service_test.go
+++ internal/service/users_service_test.go
@@ -5,18 +5,24 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/NerdBow/GrindersAPI/internal/model"
 	"github.com/joho/godotenv"
 )
 
 type MockDB struct {
-	tempHash string
+	usernames map[string]struct{}
 }
 
-func (db MockDB) SignUp(username string, password string) error {
-	if password == db.tempHash {
-		return errors.New("The hash is the same!!")
+func (db *MockDB) SignUp(username string, password string) error {
+	if _, ok := db.usernames[username]; ok {
+		return errors.New("There exist the username " + username)
 	}
+	db.usernames[username] = struct{}{}
 	return nil
+}
+
+func (db *MockDB) GetUserInfo(username string) (model.User, error) {
+	return model.User{Username: "NerdBow", Hash: "$argon2id$v=19$m=65536,t=1,p=4$c2FsdHNhbHQ$SRzrpBkxb+Cwwr5PQJL2pIGh9G59lfzlgOj3RRV73LKQYf2HycaaTY5yHimy7mnlWCY"}, nil
 }
 
 func TestMain(m *testing.M) {
@@ -28,11 +34,28 @@ func TestMain(m *testing.M) {
 	m.Run()
 }
 
+func TestCompareHash(t *testing.T) {
+	db := MockDB{}
+
+	user, _ := db.GetUserInfo("")
+
+	// Test right password
+	err := compareHash(user, "password")
+	if err != nil {
+		t.Error(err)
+	}
+
+	// Test wrong password
+	err = compareHash(user, "worng_password")
+	if err == nil {
+		t.Error(err)
+	}
+}
+
 func TestSaltGeneration(t *testing.T) {
 
-	s := NewUserService(MockDB{})
-	salt1 := s.generateSalt()
-	salt2 := s.generateSalt()
+	salt1 := generateSalt()
+	salt2 := generateSalt()
 
 	same := true
 
@@ -49,10 +72,10 @@ func TestSaltGeneration(t *testing.T) {
 }
 
 func TestHashGeneration(t *testing.T) {
-	s := NewUserService(MockDB{})
+	// Test for expected hash
 	salt := []byte("saltsalt")
 
-	hash := s.generateHash("password", salt)
+	hash := generateHash("password", salt)
 
 	expectedHash := "$argon2id$v=19$m=65536,t=1,p=4$c2FsdHNhbHQ$SRzrpBkxb+Cwwr5PQJL2pIGh9G59lfzlgOj3RRV73LKQYf2HycaaTY5yHimy7mnlWCY"
 
@@ -60,50 +83,82 @@ func TestHashGeneration(t *testing.T) {
 		t.Errorf("The two hashes are not the same.\nFuncHash: %s\nConstHash: %s", hash, expectedHash)
 	}
 
+	// Test if running the hash again will generate the same hash
+	hash2 := generateHash("password", salt)
+
+	if hash != hash2 {
+		t.Errorf("The regenerated hash is not the same as the first.\nHash1: %s\nHash2: %s", hash, hash2)
+	}
+
+	// Test that the hashs will not the be the same with different passwords
+
+	hash3 := generateHash("Password", salt)
+
+	if hash3 == hash2 {
+		t.Errorf("Hash3 is colliding with hash2 when they are not supposed to be.\nHash3: %s\nHash2: %s", hash3, hash2)
+	}
+
+	// Test different salt different hash
+
+	salt2 := []byte("saltSalt")
+
+	hash4 := generateHash("password", salt2)
+
+	if hash4 == hash {
+		t.Errorf("Hash4 is not supposed to be equal to hash1 as they have different salts\nHash4: %s\nHash1: %s", hash4, hash)
+	}
+
 }
 
 func TestSignUp(t *testing.T) {
-	s := NewUserService(MockDB{})
+	s := NewUserService(&MockDB{make(map[string]struct{})})
 
+	// Test valid username and password
 	err := s.SignUp("TestUser", "Password")
 
 	if err != nil {
 		t.Error(err)
 	}
 
+	// Test duplicate username
 	err = s.SignUp("TestUser", "Password")
 
-	if err != nil {
+	if err == nil {
 		t.Error(err)
 	}
 
 	var blankFieldsErr *BlankFieldsError
 	var invalidPasswordErr *InvalidPasswordError
 
+	// Test blank username and password
 	err = s.SignUp("", "")
 
 	if !errors.As(err, &blankFieldsErr) {
 		t.Error(err)
 	}
 
+	// Test blank password
 	err = s.SignUp("a", "")
 
 	if !errors.As(err, &blankFieldsErr) {
 		t.Error(err)
 	}
 
+	// Test blank username
 	err = s.SignUp("", "a")
 
 	if !errors.As(err, &blankFieldsErr) {
 		t.Error(err)
 	}
 
+	// Test invalid password
 	err = s.SignUp("a", "asdfjkl")
 
 	if !errors.As(err, &invalidPasswordErr) {
 		t.Error(err)
 	}
 
+	// Test one letter username
 	err = s.SignUp("a", "asdfjkl;")
 
 	if errors.As(err, &invalidPasswordErr) {
