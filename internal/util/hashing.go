758ed62e7a9f0fb05955891ec8a9137a174e31e5
NerdBow <108768934+NerdBow@users.noreply.github.com>
2025-03-16 21:43:25 -0400

    Add helper function to classes (TEMP) code is broken

--- internal/util/hashing.go
+++ internal/util/hashing.go
@@ -1,1 +1,86 @@
 package util
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+
+	"golang.org/x/crypto/argon2"
+)
+
+func GenerateSalt() []byte {
+
+	length, err := strconv.Atoi(os.Getenv("SALTLENGTH"))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	salt := make([]byte, length)
+	rand.Read(salt)
+	return salt
+}
+
+// Takes in the password string and returns the argonid2 encoded version of it.
+func GenerateHash(password string, saltBytes []byte) string {
+
+	hashTime, err := strconv.Atoi(os.Getenv("HASHTIME"))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	hashMemory, err := strconv.Atoi(os.Getenv("HASHMEMORY"))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	hashThreads, err := strconv.Atoi(os.Getenv("HASHTHREADS"))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	hashLength, err := strconv.Atoi(os.Getenv("HASHLENGHT"))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	hashBytes := argon2.IDKey([]byte(password), saltBytes, uint32(hashTime), uint32(hashMemory*1024), uint8(hashThreads), uint32(hashLength))
+
+	salt := base64.RawStdEncoding.EncodeToString(saltBytes)
+	hash := base64.RawStdEncoding.EncodeToString(hashBytes)
+
+	return fmt.Sprintf("$argon2id$v=%d$m=%d,t=%d,p=%d$%s$%s", argon2.Version, hashMemory*1024, hashTime, hashThreads, salt, hash)
+}
+
+func ParseHash(hash string) ([]byte, string, error) {
+	parsedHash := strings.Split(hash, "$")
+	salt, err := base64.RawStdEncoding.DecodeString(parsedHash[len(parsedHash)-2])
+	if err != nil {
+		return nil, "", err
+	}
+	return salt, parsedHash[len(parsedHash)-1], nil
+
+}
+
+func compareHash(user model.User, password string) error {
+	salt, _, err := ParseHash(user.Hash)
+
+	if err != nil {
+		return err
+	}
+
+	if user.Hash != GenerateHash(password, []byte(salt)) {
+		return errors.New("Invalid Password")
+	}
+
+	return nil
+}
--- internal/util/token.go
+++ internal/util/token.go
@@ -1,1 +1,108 @@
 package util
+
+import (
+	"errors"
+	"github.com/golang-jwt/jwt/v5"
+	"log"
+	"os"
+	"strconv"
+	"time"
+)
+
+func createToken(user model.User) (string, error) {
+	jwtExpDuration, err := strconv.Atoi(os.Getenv("JWTEXP"))
+
+	if err != nil {
+		log.Println("CRITICAL: JWTEXP could not be parsed from the .env file!")
+		return "", errors.New("Can not get JWT EXP time from env")
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"userId":   user.UserId,
+		"username": user.Username,
+		"exp":      time.Now().Add(time.Minute * time.Duration(jwtExpDuration)).Unix(),
+	})
+
+	jwtSecret := os.Getenv("JWTSECRET")
+
+	if jwtSecret == "" {
+		log.Println("CRITICAL: JWT secret could not be found in the .env file!")
+		return "", errors.New("Can not get secret")
+	}
+
+	signedToken, err := token.SignedString([]byte(jwtSecret))
+
+	if err != nil {
+		return "", errors.New("Could not sign")
+	}
+
+	return signedToken, nil
+
+}
+
+func getClaimsFromToken(token string) (jwt.MapClaims, error) {
+	key := func(token *jwt.Token) (any, error) {
+		secret := os.Getenv("JWTSECRET")
+		if secret == "" {
+			return nil, errors.New("Can not get secret")
+		}
+		return []byte(secret), nil
+	}
+
+	parsedToken, err := jwt.Parse(token, key, jwt.WithValidMethods([]string{"HS256"}))
+
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	if !parsedToken.Valid {
+		return nil, jwt.ErrSignatureInvalid
+	}
+
+	claims, ok := parsedToken.Claims.(jwt.MapClaims)
+
+	if !ok {
+		log.Println("Token has no claims.")
+		return nil, err
+	}
+	return claims, nil
+}
+
+func checkTokenExpiration(claims jwt.MapClaims) (bool, error) {
+	exp, err := claims.GetExpirationTime()
+
+	if err != nil {
+		return false, err
+	}
+
+	if time.Now().After(exp.Time) {
+		return false, jwt.ErrTokenExpired
+	}
+
+	return true, nil
+}
+
+func getUserFromClaims(claims jwt.MapClaims) (model.User, error) {
+	var user model.User
+
+	ok, err := checkTokenExpiration(claims)
+
+	if err != nil {
+		return user, err
+	}
+
+	user.UserId, ok = claims["userId"].(int)
+
+	if !ok {
+		return user, jwt.ErrTokenMalformed
+	}
+
+	user.Username, ok = claims["username"].(string)
+
+	if !ok {
+		return user, jwt.ErrTokenMalformed
+	}
+
+	return user, nil
+}
