734fc497396347d9172040b1fba5e2996af702f7
NerdBow <108768934+NerdBow@users.noreply.github.com>
2025-03-18 13:25:50 -0400

    Add CheckAuth middleware to check for jwt token & parse it into user model for handlers

--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -1,9 +1,80 @@
 package middleware
 
 import (
+	"context"
+	"errors"
 	"log"
 	"net/http"
+	"strconv"
+	"strings"
+
+	"github.com/NerdBow/GrindersAPI/internal/model"
+	"github.com/NerdBow/GrindersAPI/internal/util"
+	"github.com/golang-jwt/jwt/v5"
 )
+
+type ContextKey uint8
+
+var (
+	UserKey ContextKey = 1
+)
+
+func CheckAuth(handler http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		authString := r.Header.Get("Authorization")
+		parsedAuth := strings.Split(authString, " ")
+		if len(parsedAuth) != 2 {
+			HandleError(w, errors.New("JWT is not formated correctly"), http.StatusBadRequest, "JWT is not formatted correctly")
+			return
+		}
+
+		bearer, token := parsedAuth[0], parsedAuth[1]
+
+		if bearer != "Bearer" {
+			HandleError(w, errors.New("No Bearer in auth header"), http.StatusBadRequest, "JWT is not formatted correctly")
+			return
+		}
+
+		claims, err := util.GetClaimsFromToken(token)
+
+		if errors.Is(err, util.CanNotAccessJWTSecretErr) {
+			HandleError(w, err, http.StatusInternalServerError, "Internal Server Error")
+			return
+		}
+
+		if errors.Is(err, jwt.ErrTokenExpired) {
+			HandleError(w, nil, http.StatusUnauthorized, "Token Expired")
+			return
+		}
+
+		if err != nil {
+			HandleError(w, err, http.StatusBadRequest, "JWT is not formatted correctly")
+			return
+		}
+
+		var user model.User
+		var ok bool
+
+		userId, ok := claims["userId"].(string)
+
+		user.UserId, err = strconv.Atoi(userId)
+
+		if !ok || err != nil {
+			HandleError(w, errors.New("JWT does not have a userId"), http.StatusBadRequest, "JWT is not formatted correctly")
+			return
+		}
+
+		user.Username, ok = claims["username"].(string)
+
+		if !ok {
+			HandleError(w, errors.New("JWT does not have a username"), http.StatusBadRequest, "JWT is not formatted correctly")
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), UserKey, user)
+		handler(w, r.WithContext(ctx))
+	}
+}
 
 func SetLog(handler http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -20,7 +91,9 @@ func SetHeader(handler http.HandlerFunc) http.HandlerFunc {
 }
 
 func HandleError(w http.ResponseWriter, err error, statusCode int, message string) {
-	log.Printf("%s\n", err)
+	if err != nil {
+		log.Printf("%s\n", err)
+	}
 	w.WriteHeader(statusCode)
 	w.Write([]byte(message))
 }
