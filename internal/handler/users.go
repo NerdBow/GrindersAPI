99bd9d218d8937db233cbbb609c03c906f6332e4
NerdBow <108768934+NerdBow@users.noreply.github.com>
2025-03-27 19:26:41 -0400

    Remove logic from UserLog handlers

--- internal/handler/users.go
+++ internal/handler/users.go
@@ -3,13 +3,10 @@ package handler
 import (
 	"encoding/json"
 	"errors"
-	"fmt"
 	"log"
 	"net/http"
-	"strconv"
 
 	"github.com/NerdBow/GrindersAPI/internal/middleware"
-	"github.com/NerdBow/GrindersAPI/internal/model"
 	"github.com/NerdBow/GrindersAPI/internal/service"
 )
 
@@ -113,55 +110,6 @@ func HandleUserSignUp(s service.UserService) http.HandlerFunc {
 // Returns http handler func.
 func HandleUserLogPost(s service.UserLogService) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		decoder := json.NewDecoder(r.Body)
-		decoder.DisallowUnknownFields()
-		var requestLog model.Log
-		err := decoder.Decode(&requestLog)
-
-		fmt.Println(r.PathValue("id"))
-
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			w.Write([]byte("400 Bad request"))
-			fmt.Println(err)
-			return
-		}
-
-		if len(requestLog.Validate()) != 0 {
-			fmt.Println(requestLog.Validate())
-			fmt.Println("Log was not valid")
-			w.WriteHeader(http.StatusBadRequest)
-			w.Write([]byte("400 Bad request"))
-			return
-		}
-
-		logId, err := s.AddUserLog(requestLog)
-
-		if err != nil {
-			fmt.Println(err)
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte("500 Internal Server Error"))
-			return
-		}
-
-		data := struct {
-			Id int `json:"id"`
-		}{logId}
-
-		dataBytes, err := json.Marshal(data)
-
-		if err != nil {
-			fmt.Println(err)
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte("500 Internal Server Error"))
-			return
-		}
-
-		w.Header().Set("Content-Type", "application/json")
-
-		w.Write(dataBytes)
-
-		fmt.Println("Post")
 	}
 }
 
@@ -170,53 +118,6 @@ func HandleUserLogPost(s service.UserLogService) http.HandlerFunc {
 // Returns http handler func.
 func HandleUserLogGet(s service.UserLogService) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		// This can be factored out into a new function
-		requestedId := r.URL.Path[5:]
-		requestedUser := r.PathValue("username")
-
-		logId, err := strconv.Atoi(requestedId)
-
-		if err != nil {
-			fmt.Println("Get method attempted with:", requestedId)
-			w.WriteHeader(http.StatusBadRequest)
-			w.Write([]byte("400 Bad Request"))
-			return
-		}
-
-		userId, err := strconv.Atoi(requestedUser)
-
-		if err != nil {
-			fmt.Println("Get method attempted with:", userId)
-			w.WriteHeader(http.StatusBadRequest)
-			w.Write([]byte("400 Bad Request"))
-			return
-		}
-
-		queriedLog, err := s.GetUserLog(userId, logId)
-
-		if err != nil {
-			fmt.Println(err)
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte("500 Internal Server Error"))
-			return
-		}
-
-		dataBytes, err := json.Marshal(queriedLog)
-
-		if err != nil {
-			fmt.Println(err)
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte("500 Internal Server Error"))
-			return
-		}
-
-		w.Header().Set("Content-Type", "application/json")
-
-		w.Write(dataBytes)
-
-		fmt.Printf("Fetch Log %d\n", logId)
-
-		fmt.Println("Get")
 	}
 }
 
@@ -225,48 +126,6 @@ func HandleUserLogGet(s service.UserLogService) http.HandlerFunc {
 // Returns http handler func.
 func HandleUserLogUpdate(s service.UserLogService) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		decoder := json.NewDecoder(r.Body)
-		decoder.DisallowUnknownFields()
-		var requestLog model.Log
-		err := decoder.Decode(&requestLog)
-
-		if err != nil || requestLog.Id == 0 {
-			w.WriteHeader(http.StatusBadRequest)
-			w.Write([]byte("400 Bad request"))
-			fmt.Println(err)
-			return
-		}
-
-		result, err := s.UpdateUserLog(requestLog)
-
-		if err != nil || !result {
-			fmt.Println(result)
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte("500 Internal Server Error"))
-			fmt.Println(err)
-			return
-		}
-
-		data := struct {
-			Id int `json:"id"`
-		}{requestLog.Id}
-
-		dataBytes, err := json.Marshal(data)
-
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte("500 Internal Server Error"))
-			fmt.Println(err)
-			return
-		}
-
-		w.Header().Set("Content-Type", "application/json")
-
-		w.Write(dataBytes)
-
-		fmt.Printf("Update Log %d", requestLog.Id)
-
-		fmt.Println("Put")
 	}
 }
 
@@ -275,45 +134,5 @@ func HandleUserLogUpdate(s service.UserLogService) http.HandlerFunc {
 // Returns http handler func.
 func HandleUserLogDelete(s service.UserLogService) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		requestedId := r.URL.Path[5:]
-
-		logId, err := strconv.Atoi(requestedId)
-
-		if err != nil {
-			fmt.Println("Delete method attempted with:", requestedId)
-			w.WriteHeader(http.StatusBadRequest)
-			w.Write([]byte("400 Bad request"))
-			return
-		}
-
-		result, err := s.DeleteUserLog(0, logId)
-
-		if err != nil || !result {
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte("500 Internal Server Error"))
-			fmt.Println(err)
-			return
-		}
-
-		data := struct {
-			Id int `json:"id"`
-		}{logId}
-
-		dataBytes, err := json.Marshal(data)
-
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte("500 Internal Server Error"))
-			fmt.Println(err)
-			return
-		}
-
-		w.Header().Set("Content-Type", "application/json")
-
-		w.Write(dataBytes)
-
-		fmt.Printf("Delete Log %d", logId)
-
-		fmt.Println("Delete")
 	}
 }
